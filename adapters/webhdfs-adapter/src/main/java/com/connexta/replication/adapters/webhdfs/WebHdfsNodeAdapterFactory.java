be9829688aa8197eb5baa3263ae86965814a5243
Chris Lange <chris.lange@connexta.com>
2020-06-25 11:27:10 -0400

    added constructor and comments to factory

--- adapters/webhdfs-adapter/src/main/java/com/connexta/replication/adapters/webhdfs/WebHdfsNodeAdapterFactory.java
+++ adapters/webhdfs-adapter/src/main/java/com/connexta/replication/adapters/webhdfs/WebHdfsNodeAdapterFactory.java
@@ -16,9 +16,50 @@ package com.connexta.replication.adapters.webhdfs;
 import com.connexta.replication.api.NodeAdapter;
 import com.connexta.replication.api.NodeAdapterFactory;
 import com.connexta.replication.api.data.SiteType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+
 import java.net.URL;
 
+/**
+ * Factory for creating {@link WebHdfsNodeAdapter} instances
+ */
 public class WebHdfsNodeAdapterFactory implements NodeAdapterFactory {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(WebHdfsNodeAdapterFactory.class);
+
+  /**
+   * Factory used to create HTTP request objects
+   */
+  private final SimpleClientHttpRequestFactory requestFactory;
+
+  /**
+   * The connection timeout for any clients created by this factory
+   */
+  private final int connectionTimeout;
+
+  /**
+   * The receive timeout for any clients created by this factory
+   */
+  private final int receiveTimeout;
+
+  /**
+   * Creates a WebHdfsNodeAdapter
+   *
+   * @param requestFactory a factory used to create HTTP request objects
+   * @param connectionTimeout the connection timeout for any clients created by this factory
+   * @param receiveTimeout the receive timeout for any clients created by this factory
+   */
+  public WebHdfsNodeAdapterFactory(SimpleClientHttpRequestFactory requestFactory, int connectionTimeout, int receiveTimeout) {
+    this.requestFactory = requestFactory;
+    this.connectionTimeout = connectionTimeout;
+    this.receiveTimeout = receiveTimeout;
+    LOGGER.debug(
+            "Created a WebHdfsNodeAdapterFactory with a connection timeout of {}ms and a receive timeout of {}ms",
+            connectionTimeout,
+            receiveTimeout);
+  }
 
   @Override
   public NodeAdapter create(URL url) {
