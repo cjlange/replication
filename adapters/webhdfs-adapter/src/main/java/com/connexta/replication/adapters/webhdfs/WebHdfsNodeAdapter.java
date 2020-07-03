cceabd05a89b882fe53a9bd9c5c37ba9ce64fff1
Chris Lange <chris.lange@connexta.com>
2020-07-03 17:50:30 -0400

    completed implementation of query method for DDF approach

--- adapters/webhdfs-adapter/src/main/java/com/connexta/replication/adapters/webhdfs/WebHdfsNodeAdapter.java
+++ adapters/webhdfs-adapter/src/main/java/com/connexta/replication/adapters/webhdfs/WebHdfsNodeAdapter.java
@@ -16,6 +16,8 @@ package com.connexta.replication.adapters.webhdfs;
 import com.connexta.replication.adapters.webhdfs.filesystem.DirectoryListing;
 import com.connexta.replication.adapters.webhdfs.filesystem.FileStatus;
 import com.connexta.replication.adapters.webhdfs.filesystem.IterativeDirectoryListing;
+import com.connexta.replication.api.AdapterException;
+import com.connexta.replication.api.AdapterInterruptedException;
 import com.connexta.replication.api.NodeAdapter;
 import com.connexta.replication.api.ReplicationException;
 import com.connexta.replication.api.data.CreateRequest;
@@ -28,15 +30,25 @@ import com.connexta.replication.api.data.ResourceRequest;
 import com.connexta.replication.api.data.ResourceResponse;
 import com.connexta.replication.api.data.UpdateRequest;
 import com.connexta.replication.api.data.UpdateStorageRequest;
+import com.connexta.replication.data.MetadataAttribute;
+import com.connexta.replication.data.MetadataImpl;
+import com.connexta.replication.data.QueryResponseImpl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import org.slf4j.Logger;
@@ -63,6 +75,17 @@ public class WebHdfsNodeAdapter implements NodeAdapter {
   private static final String HTTP_FILE_SYSTEM_ACTION_ALL = "rwx";
 
   private static final String HTTP_START_AFTER_KEY = "startAfter";
+
+  private static final String METADATA_ATTRIBUTE_NAME_ID = "id";
+  private static final String METADATA_ATTRIBUTE_NAME_TITLE = "title";
+  private static final String METADATA_ATTRIBUTE_NAME_CREATED = "created";
+  private static final String METADATA_ATTRIBUTE_NAME_MODIFIED = "modified";
+  private static final String METADATA_ATTRIBUTE_NAME_RESOURCE_URI = "resource-uri";
+  private static final String METADATA_ATTRIBUTE_NAME_RESOURCE_SIZE = "resource-size";
+  private static final String METADATA_ATTRIBUTE_TYPE_STRING = "string";
+  private static final String METADATA_ATTRIBUTE_TYPE_DATE = "date";
+
+  private static final int UUID_VERSION_INDEX = 14;
 
   /** The address of the REST API for the Hadoop instance */
   private final URL webHdfsUrl;
@@ -121,7 +144,115 @@ public class WebHdfsNodeAdapter implements NodeAdapter {
   public QueryResponse query(QueryRequest queryRequest) {
     List<FileStatus> filesToReplicate = getFilesToReplicate(queryRequest.getModifiedAfter());
 
-    return null;
+    filesToReplicate.sort(Comparator.comparing(FileStatus::getModificationTime));
+
+    List<Metadata> results = new ArrayList<>();
+
+    for (FileStatus file : filesToReplicate) {
+      results.add(createMetadata(file));
+    }
+
+    return new QueryResponseImpl(results, e -> wrapException("Failed to query remote system", e));
+  }
+
+  /**
+   * Creates {@link MetadataAttribute}s to use in creating a {@link MetadataImpl} object for a file
+   * being replicated.
+   *
+   * @param fileStatus information about the file being replicated
+   * @return a {@link MetadataImpl} object for the file being replicated
+   */
+  Metadata createMetadata(FileStatus fileStatus) {
+    // TODO: 7/3/20 This approach is for creation in DDF, not Ion
+
+    Map<String, MetadataAttribute> metadataAttributes = new HashMap<>();
+
+    String fileUrl = getWebHdfsUrl().toString() + fileStatus.getPathSuffix();
+    LOGGER.debug("Creating Metadata object from file at: {}", fileUrl);
+
+    Date modificationTime = fileStatus.getModificationTime();
+    String id = getVersion4Uuid(fileUrl, modificationTime);
+
+    MetadataAttribute idAttribute =
+        new MetadataAttribute(
+            METADATA_ATTRIBUTE_NAME_ID,
+            METADATA_ATTRIBUTE_TYPE_STRING,
+            Collections.singletonList(id),
+            Collections.emptyList());
+    metadataAttributes.put(METADATA_ATTRIBUTE_NAME_ID, idAttribute);
+
+    MetadataAttribute titleAttribute =
+        new MetadataAttribute(
+            METADATA_ATTRIBUTE_NAME_TITLE,
+            METADATA_ATTRIBUTE_TYPE_STRING,
+            Collections.singletonList(fileStatus.getPathSuffix()),
+            Collections.emptyList());
+    metadataAttributes.put(METADATA_ATTRIBUTE_NAME_TITLE, titleAttribute);
+
+    MetadataAttribute createdAttribute =
+        new MetadataAttribute(
+            METADATA_ATTRIBUTE_NAME_CREATED,
+            METADATA_ATTRIBUTE_TYPE_DATE,
+            Collections.singletonList(modificationTime.toString()),
+            Collections.emptyList());
+    metadataAttributes.put(METADATA_ATTRIBUTE_NAME_CREATED, createdAttribute);
+
+    MetadataAttribute modifiedAttribute =
+        new MetadataAttribute(
+            METADATA_ATTRIBUTE_NAME_MODIFIED,
+            METADATA_ATTRIBUTE_TYPE_DATE,
+            Collections.singletonList(modificationTime.toString()),
+            Collections.emptyList());
+    metadataAttributes.put(METADATA_ATTRIBUTE_NAME_MODIFIED, modifiedAttribute);
+
+    MetadataAttribute resourceUriAttribute =
+        new MetadataAttribute(
+            METADATA_ATTRIBUTE_NAME_RESOURCE_URI,
+            METADATA_ATTRIBUTE_TYPE_STRING,
+            Collections.singletonList(fileUrl),
+            Collections.emptyList());
+    metadataAttributes.put(METADATA_ATTRIBUTE_NAME_RESOURCE_URI, resourceUriAttribute);
+
+    MetadataAttribute lengthAttribute =
+        new MetadataAttribute(
+            METADATA_ATTRIBUTE_NAME_RESOURCE_SIZE,
+            METADATA_ATTRIBUTE_TYPE_STRING,
+            Collections.singletonList(String.valueOf(fileStatus.getLength())),
+            Collections.emptyList());
+    metadataAttributes.put(METADATA_ATTRIBUTE_NAME_RESOURCE_SIZE, lengthAttribute);
+
+    Metadata metadata = new MetadataImpl(metadataAttributes, Map.class, id, modificationTime);
+
+    try {
+      metadata.setResourceUri(new URI(fileUrl));
+      metadata.setResourceModified(modificationTime);
+      metadata.setResourceSize(fileStatus.getLength());
+
+      return metadata;
+    } catch (URISyntaxException e) {
+      throw new ReplicationException("Unable to create a URI from the file's URL.", e);
+    }
+  }
+
+  /**
+   * Using the file URL and modification time as input, a version-3 UUID is generated which is then
+   * modified to appear as a version-4 UUID by changing the version number in the UUID from 3 to 4.
+   * The URL is not guaranteed to be unique if a file is removed and replaced by another file with
+   * the same URL. Because of this, the URL is combined with the modification time to serve as input
+   * for UUID generation.
+   *
+   * @param fileUrl the full URL of the file
+   * @param modificationTime modification time of the file
+   * @return a {@code String} representing a version-4 UUID
+   */
+  private String getVersion4Uuid(String fileUrl, Date modificationTime) {
+    String input = String.format("%s_%d", fileUrl, modificationTime.getTime());
+    String version3Uuid = UUID.nameUUIDFromBytes(input.getBytes()).toString();
+    StringBuilder version4Uuid = new StringBuilder(version3Uuid);
+    version4Uuid.setCharAt(UUID_VERSION_INDEX, '4');
+
+    LOGGER.debug("UUID for {} is {}", fileUrl, version4Uuid);
+    return version4Uuid.toString();
   }
 
   /**
@@ -248,6 +379,30 @@ public class WebHdfsNodeAdapter implements NodeAdapter {
   @Override
   public boolean updateResource(UpdateStorageRequest updateStorageRequest) {
     return false;
+  }
+
+  private AdapterException wrapException(String msg, Exception e) {
+    Throwable t = e;
+
+    if (t instanceof UndeclaredThrowableException) {
+      t = ((UndeclaredThrowableException) t).getUndeclaredThrowable();
+    }
+    if (t instanceof InterruptedIOException) {
+      return new AdapterInterruptedException((InterruptedIOException) t);
+    } else if (t instanceof InterruptedException) {
+      return new AdapterInterruptedException((InterruptedException) t);
+    } else if (t instanceof AdapterInterruptedException) {
+      return (AdapterInterruptedException) t;
+    } else if (t instanceof Error) {
+      throw (Error) t;
+    } else if (t instanceof AdapterException) {
+      // re-write the exception with our message while preserving its stack trace
+      final AdapterException ae = new AdapterException(msg, t.getCause());
+
+      ae.setStackTrace(t.getStackTrace());
+      throw ae;
+    }
+    return new AdapterException(msg, (Exception) t);
   }
 
   @Override
